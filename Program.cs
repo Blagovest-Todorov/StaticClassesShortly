42c213ad6473288d2eb17700f12734876b4d177b
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-04-28 10:57:23 +0300

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StaticClasses
+{
+   static class SoftUniMaths
+    {  //static class -class -we can not make instances, cariables, objects of teh class 
+        // static classes can not be instantiated, all members must be static , otherwise compiler Error
+        //static class is sealed -> can not be inherited
+        //static class can not ihnerit from other classes, , static members can be accessed through class name
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(SoftUniMaths.Add(5, 6));
+        }
+
+        public static int Add(int a, int b)  
+        {
+            return a + b;
+        }
+        public static int multiply(int a, int b) 
+        {
+            return a * b;
+        } // Static Method is hung directly to the class, not tot the curr instance
+        // MAth, Console, TimeZone
+        // Static classes are global, simplify the code, more readble, better perfoamance
+    }
+}
